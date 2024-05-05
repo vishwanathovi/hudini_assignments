678c10cfff1ab965b7c64fe5b00456a11a79c24c
Tharun Suresh <tharun.suresh@hudini.io>
2024-05-05 21:11:02 +0530

    first commit

---
+++ javascript/Day1/Assignment2.js
@@ -0,0 +1,78 @@
+// Question 1: Write a function that calculates the sum of odd numbers greater than 10 and less than 30. Print the sum at the end.
+function oddCalculations()
+{
+    let sum=0;
+    for (let i=11;i<30;i++)
+    {
+        if(i%2!=0)
+        {
+            sum=sum+i;
+        }
+    }
+    return sum;
+}
+console.log(oddCalculations());
+// Question 2: Write a function that takes a number between 1-20 and prints the multiplication table of the same. Each entry should be in the format `2x1=2`
+function table(number)
+{
+    if(number>1 && number<20){
+    for (let i=1;i<=10;i++)
+    {
+        console.log(number+'x'+i+'='+i*number);
+    }
+}
+}
+
+table(4);
+// Question 3: Create a function that receives an array of numbers and returns an array containing only the positive numbers
+function positiveArray(numbers)
+{
+    let size=numbers.length;
+    let arrays=[]
+    for(let i=0;i<size;i++)
+    {
+        if(numbers[i]>0)
+        {
+            arrays.push(numbers[i])
+        }
+    }
+    console.log(arrays);
+}
+// Question 4: Write a function that finds the maximum number in an array of numbers and prints it.
+function maxNumber(array1)
+{
+    let size=array1.length;
+    let max=array1[0];
+    for(let i=1;i<size;i++)
+    {
+        if(array1[i]>max)
+        {
+            max=array1[i];
+        }
+    }
+    return max;
+}
+// Question 5: Write a program that prints the numbers from 1 to n. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz". For numbers that are multiples of both three and five, print "FizzBuzz".
+
+function FizzBuzz(number1)
+{
+    for(let i=1;i<=number1;i++)
+    {
+        if(i%3==0 && i%5==0)
+        {
+            console.log("Fizzbuzz");
+        }
+        else if(i%3==0)
+        {
+            console.log('Fizz');
+        }
+        else if(i%5==0)
+        {
+            console.log('Buzz');
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+FizzBuzz(15)
